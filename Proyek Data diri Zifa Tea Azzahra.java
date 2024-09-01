46769d095bdc68596d627dbac4c2d782622b69d0
Zifa Tea Azzahra <zifateaazzahra@gmail.com>
2024-09-01 22:35:07 +0700

    Add files via upload

---
+++ Proyek Data diri Zifa Tea Azzahra.java
@@ -0,0 +1,14 @@
+public class DataDiri {
+    public static void main(String[] args) {
+        
+        String nama = "Zifa Tea Azzahra";
+        String npm = "2320506031";
+        String alamat = "Pinggirejo Wates_Rt004_Rw007";
+        String noHp = "085710045896";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("No HP: " + noHp);
+    }
+}
