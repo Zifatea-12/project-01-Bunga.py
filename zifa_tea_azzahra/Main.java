7e170ee5a71fac29bd334c07f9d049fa256d7fbd
Zifa Tea Azzahra <zifateaazzahra@gmail.com>
2024-09-30 19:35:13 +0700

    Add files via upload

---
+++ zifa_tea_azzahra/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.responsiuts.zifa_tea_azzahra;
+
+/**
+ *
+ * @author zifat
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Membuat objek Elektronik
+        Produk laptop = new Elektronik("Laptop", 15000000, 2);
+        laptop.tampilkanInfo();
+
+        System.out.println();
+
+        // Membuat objek PegawaiTetap
+        Pegawai Mahendra = new PegawaiTetap("Mahendra", 5000000, 1000000);
+        Mahendra.tampilkanInfo();
+
+        System.out.println();
+
+        // Polimorfisme
+        Produk snack = new Makanan("Snack", 15000, "2023-12-30");
+        Pegawai Nathan = new PegawaiKontrak("Nathan", 3000000, 12);
+
+        snack.tampilkanInfo();
+        System.out.println();
+        Nathan.tampilkanInfo();
+    }
+}
